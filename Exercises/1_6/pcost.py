07fe181128a69e83baa65cd57015f70584b7303d
Mitchell Krieger <mitkrieger@gmail.com>
2023-12-31 19:48:12 -0500

    exercise 1.5 and 1.6

---
+++ Exercises/1_6/pcost.py
@@ -0,0 +1,21 @@
+
+
+def portfolio_cost(filename):
+    with open(filename, 'r') as f:
+        data = []
+        for line in f:
+            data.append(line.split())
+
+        cost = 0
+        for stock in data:
+            try:
+                cost += float(stock[1]) * float(stock[2])
+            except Exception as e:
+                print(f'{e} for {stock}')
+
+        return cost
+
+
+if __name__ == '__main__':
+    print(portfolio_cost(
+        '/Users/mitchellkrieger/Documents/GitHub/python-mastery/Data/portfolio2.dat'))
